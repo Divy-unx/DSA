f9314805b7e66bbdcd887b7fa8738d9c323bc554
Divyanshu Mishra <divyanshu-4484@users.noreply.github.com>
2026-05-04 10:37:01 +0530

    Added Bar Chart optimized problem DSA in Java

---
+++ src/Arrays/BarChart.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Scanner;
+public class BarChart{
+    static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        for(int floor = max; floor > 0; floor-- ){
+            for(int build : arr){
+                if(build >= floor){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

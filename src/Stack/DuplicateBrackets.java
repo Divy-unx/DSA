c23577b77615db342e6f3504f9826776e9dec166
Divyanshu Mishra <divyanshu-4484@users.noreply.github.com>
2026-05-29 18:38:37 +0530

    Solved Duplicate Brackets problem in stack

---
+++ src/Stack/DuplicateBrackets.java
@@ -0,0 +1,31 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DuplicateBrackets {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i =0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == ')'){
+                if(stack.peek() == '(') {
+                    System.out.println(true);
+                    return;
+                }else{
+                    while(stack.peek() != '('){
+                        stack.pop();
+                    }
+                }
+                stack.pop();
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+        System.out.println(false);
+    }
+}
